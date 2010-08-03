82e000fe6632533b76cbb77475cd64804111af2e
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2010-08-03 12:42:55 +0200

    No more language warnings. Now the interesting work can begin.

--- src/alice/tuprolog/ui/Automaton.java
+++ src/alice/tuprolog/ui/Automaton.java
@@ -13,63 +13,61 @@ package alice.tuprolog.ui;
  * this abstract class is the base class for
  * implementing automaton abstraction
  *
- * automaton state behaviour is expressed
+ * automaton state behavior is expressed
  * in public method and the become method
  * allows to move computation from state to state
  *
  * method representing state must be public
- * (to allow correct behaviour of reflection)
- *
+ * (to allow correct behavior of reflection)
  */
 public abstract class Automaton implements Runnable {
 
-    /** method name representing state behaviour */
-    protected String state="boot";
+    /** method name representing state behavior */
+    protected String state = "boot";
 
     /** arguments value eventually associated to state transition */
-    protected Object[] arguments=null;
+    protected Object[] arguments = null;
 
     /** arguments class eventually associated to state transition */
-    protected Class[] argType;
+    protected Class<?>[] argType;
 
-    public Automaton(){
+    public Automaton() {
         try {
-            argType=new Class[]{ Class.forName("[Ljava.lang.Object;") };
-        } catch (Exception ex){
+            argType = new Class[]{ Class.forName("[Ljava.lang.Object;") };
+        } catch (Exception ex) {
         }
     }
 
-    protected void become(String s){
-        if (!state.equals("end")){
-            state=s;
-            arguments=null;
+    protected void become(String s) {
+        if (!state.equals("end")) {
+            state = s;
+            arguments = null;
         }
     }
 
-    protected void become(String s, Object[] args){
-        if (!state.equals("end")){
-            state=s;
-            arguments=args;
+    protected void become(String s, Object[] args) {
+        if (!state.equals("end")) {
+            state = s;
+            arguments = args;
         }
     }
 
     /** boot state of automaton */
     public abstract void boot();
 
-    public void run(){
-        while (true){
+    public void run() {
+        while (true) {
             try {
-                if (!state.equals("end")){
-                    if (arguments==null){
-                        this.getClass().getDeclaredMethod(state,null).invoke(this,null);
-                    } else {
-                        this.getClass().getDeclaredMethod(state,argType).invoke(this,arguments);
-                    }
-                } else {
+                if (!state.equals("end"))
+                    if (arguments == null)
+                        this.getClass().getDeclaredMethod(state, (Class<?>[]) null).invoke(this, (Object[]) null);
+                    else
+                        this.getClass().getDeclaredMethod(state, argType).invoke(this, arguments);
+                else {
                     end();
                     break;
                 }
-            } catch (Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace();
                 error();
             }
@@ -77,19 +75,19 @@ public abstract class Automaton implements Runnable {
     }
 
     /** idle state */
-    public void idle(){
+    public void idle() {
         try {
             wait();
-        } catch (Exception ex){
+        } catch (Exception ex) {
         }
     }
 
     /** shutdown state */
-    public void end(){
+    public void end() {
     }
 
     /** error state */
-    public void error(){
+    public void error() {
         become("end");
     }
 }
