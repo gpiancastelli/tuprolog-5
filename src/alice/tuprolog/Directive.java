e2d3ace352109779c6c2d15e7196f24b76724f14
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2010-08-04 16:32:58 +0200

    Specify the intended target of annotations.

--- src/alice/tuprolog/Directive.java
+++ src/alice/tuprolog/Directive.java
@@ -1,14 +1,17 @@
 package alice.tuprolog;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Marks a Java method representing a Prolog directive in a library.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Directive {
 	
 	String value();
--- src/alice/tuprolog/Predicate.java
+++ src/alice/tuprolog/Predicate.java
@@ -1,16 +1,19 @@
 package alice.tuprolog;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Marks a Java method representing a Prolog predicate in a library.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Predicate {
 	
 	String value();
-
+	
 }
